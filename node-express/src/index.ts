0b2e95b7f2f5d1dc050e309b8e31ad030aa19c41
Adhi Ravishankar <adhiravishankar@gmail.com>
2023-03-03 10:48:47 -0700

    Fix node resolution issue

--- node-express/src/index.ts
+++ node-express/src/index.ts
@@ -1,13 +1,24 @@
-import express = require("express");
+
 import {routes} from "./routes";
-import bodyParser = require("body-parser");
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import morgan from "morgan";
+import express from "express";
+import bodyParser from "body-parser";
 
+dotenv.config();
 
+const mongoString = process.env.MONGODB_URL
+await mongoose.connect(mongoString);
+const database = mongoose.connection
+console.log(database);
 const app = express();
+app.use(morgan('dev'))
+
 const urlEncodedParser = bodyParser.urlencoded();
 
 routes(app, urlEncodedParser);
 
 
 
-app.listen(5000);
+app.listen(5000, () => console.log('Listening on port 5000'));
