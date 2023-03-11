784040ce19cbaee901ca59ba24f32c8adb2bdb90
Adhi Ravishankar <adhiravishankar@gmail.com>
2023-03-11 12:33:46 -0700

    Bugfix create-env.js

--- angular/create-env.js
+++ angular/create-env.js
@@ -1,21 +1,15 @@
 const fse = require('fs-extra');
 const dotenv = require('dotenv');
 
-// Load environment variables from .env file
-const envConfig = dotenv.parse(fse.readFileSync('.env'));
+dotenv.config();
 
-// Convert environment variables to object
-const envObject = Object.keys(envConfig).reduce((prev, next) => {
-  prev[next] = envConfig[next];
-  return prev;
-}, {});
 
 // Generate environment.ts file with environment variables
 fse.writeFileSync(
   './src/environments/environment.ts',
   `export const environment = {
-    API_URL: '${ envObject.API_URL }',
-    PRODUCTION: ${ envObject.PRODUCTION },
+    API_URL: '${ process.env.API_URL }',
+    PRODUCTION: ${ process.env.PRODUCTION },
 };
 `
 );
